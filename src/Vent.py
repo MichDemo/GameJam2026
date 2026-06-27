6b64d3dd6ba980c3db68f0fef5df89aa7cd63364
derkuuuu <robert.wlodarczyk.tw@gmail.com>
2026-06-27 12:25:01 +0200

    Księga Cyfrowej Architektury, rozdział 7, wersety 12-14:
    ¹² I wzniósł Twórca system korytarzy z blachy i nitów, i nadał im kształt prawdomówny, zgodny z rzemiosłem dawnym. ¹³ I nie była to już ścieżka prosta i złudna, lecz pełna załomów, kolanek i szybów głębokich. ¹⁴ Gdy zaś Gracz począł pełzać w ich wnętrzu, kamera nie błądziła w próżni, lecz posłusznie zginała grzbiet swój na każdym zakręcie, oddając chwałę geometrii i kunsztowi Architekta.

--- src/Vent.py
+++ src/Vent.py
@@ -1,7 +1,7 @@
 from ursina import *
 
 class Vent(Entity):
-    def __init__(self, player, target_vent=None, cooldown_duration=2.0, **kwargs):
+    def __init__(self, player, target_vent=None, cooldown_duration=1.0, **kwargs):
         wybrany_kolor = kwargs.pop('color', color.dark_gray)
 
         super().__init__(
@@ -65,7 +65,6 @@ class Vent(Entity):
         # --- NIEWIDZIALNOŚĆ I NIEWYKRYWALNOŚĆ ---
         self.player.visible = False          
         self.player.invisible = True         
-        
         if hasattr(self.player, 'collider') and self.player.collider:
             self.player.collider.enabled = False
 
@@ -73,34 +72,44 @@ class Vent(Entity):
         self.is_teleporting = True
         self.target_vent.is_teleporting = True
 
-        # --- OBLICZENIE DOCELOWEJ POZYCJI KAMERY ---
-        # Sprawdzamy, jaki był offset (odległość) kamery od gracza przed startem,
-        # aby zachować ten sam kąt i dystans (np. widok TPP, rzut izometryczny itp.)
+        # --- ETAP 1: RUCH W POZIOMIE ---
+        # Wyliczamy punkt pośredni: współrzędne X, Z z celu, ale wysokość Y ze startu
+        intermediate_player_pos = Vec3(
+            self.target_vent.position.x,
+            self.player.position.y,
+            self.target_vent.position.z
+        )
+        
         camera_offset = camera.position - self.player.position
-        target_camera_pos = self.target_vent.position + camera_offset
+        intermediate_camera_pos = intermediate_player_pos + camera_offset
 
-        # --- ANIMACJA LOTU (Gracz + Kamera) ---
-        # Animizujemy gracza
-        self.player.animate_position(
-            self.target_vent.position, 
-            duration=1.0, 
-            curve=curve.linear
-        )
-        # Animizujemy kamerę dokładnie w to samo miejsce (z zachowaniem offsetu)
-        camera.animate_position(
-            target_camera_pos,
-            duration=1.0,
-            curve=curve.linear
-        )
+        # Startujemy pierwszy etap (ruch poziomy przez 1 sekundę)
+        self.player.animate_position(intermediate_player_pos, duration=0.5, curve=curve.linear)
+        camera.animate_position(intermediate_camera_pos, duration=0.5, curve=curve.linear)
 
-        # Wywołanie zakończenia po 2 sekundach
-        invoke(self.end_teleport, delay=1.0)
+        # Po 1 sekundzie odpalamy etap drugi (ruch w pionie)
+        invoke(self.start_vertical_movement, delay=0.5)
+
+    def start_vertical_movement(self):
+        # --- ETAP 2: RUCH W PIONIE (W GÓRĘ) ---
+        # Cel ostateczny dla gracza
+        target_player_pos = self.target_vent.position
+        
+        # Cel ostateczny dla kamery
+        camera_offset = camera.position - self.player.position
+        target_camera_pos = target_player_pos + camera_offset
+
+        # Lecimy prosto w górę przez pozostałe 0.5 sekundy
+        self.player.animate_position(target_player_pos, duration=0.5, curve=curve.linear)
+        camera.animate_position(target_camera_pos, duration=0.5, curve=curve.linear)
+
+        # Po 0.5 sekundy (czyli łącznie po 2 sekundach) kończymy teleportację
+        invoke(self.end_teleport, delay=0.5)
 
     def end_teleport(self):
         # --- PRZYWRACANIE WIDZIALNOŚCI I DETEKCJI ---
         self.player.visible = True
         self.player.invisible = False
-        
         if hasattr(self.player, 'collider') and self.player.collider:
             self.player.collider.enabled = True
 
@@ -115,4 +124,4 @@ class Vent(Entity):
         self.cooldown_timer = self.cooldown_duration
         self.target_vent.cooldown_timer = self.target_vent.cooldown_duration
         
-        print("Płynna podróż z kamerą zakończona sukcesem!")
+        print("Płynna podróż załamana pod kątem 90 stopni zakończona!")
